5b0bfe4b9e10f12c964ed5a4558be5433aef3dbc
oyerindedaniel <oyerinde.daniel@yahoo.com>
2025-06-22 17:10:24 +0100

    removed console logs

--- src/contexts/chat-context.tsx
+++ src/contexts/chat-context.tsx
@@ -109,7 +109,7 @@ export function ChatProvider({ children }: { children: React.ReactNode }) {
       const guestConversation = guestStorage.getConversation(
         currentConversationId
       );
-      console.log({ guestConversation });
+
       return guestConversation?.messages || [];
     }
 
